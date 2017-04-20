4be21f67f65e63e042c0427da50ab9406a7129f6
KirutoChan <kkristinaityte@gmail.com>
2017-04-20 18:53:21 +0300

    correct 3rd exercise for all numbers

--- leo3.py
+++ leo3.py
@@ -3,30 +3,41 @@ import math
 
 # What is the largest prime factor of the number 600851475143 ?
 
-def primenumbers(n):
-	list = []
+
+def prime_numbers(n):
+	list = []					 
+	n = int(math.sqrt(n))			# doesn't fit for ex. n = 1002 (should be 167 the largest, but it's three)
 	
-	if n > 100:
-		n = int(math.sqrt(n))
-	
-	for i in range(1, n):            # if n is small, then it seems prime numbers under sqrt(n) are not enough
+	for i in range(1, n + 1):           
 		if i % 2 != 0 or i == 2:
-			list.append(i)
+			list.append(i)		
 	return list
 
 def prime(n):
-	used = []
-	for i in primenumbers(n):
+	product = 1
+	list_prime = prime_numbers(n)
+
+	prime_factors = []
+	for i in list_prime:
 		new = n / i
 		if n % i == 0 and i != 1:
-			used.append(i)
+			prime_factors.append(i)
 			n = new
-	print (used)
-	return used
+	if prime_factors == []:				# checks if the number is already prime itself
+		return [n]
+	if n % 2 != 0:						# because we check only that prime numbers which are under sqrt(n), the last n isn's compared to list_prime (it's larger), so we check if it is prime or not, and if yes, add to prime factors list
+		prime_factors.append(int(n))
+	return prime_factors, max(prime_factors)
 
 
-#print (prime(600851475143)) 
-print (prime(150)) 
+print (prime(600851475143)) 
+print (prime(100000000002)) 
+print (prime(1002)) 
+print (prime(6)) 
+
+
+
+
 
 
 
