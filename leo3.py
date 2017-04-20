ec726292dc5233d56191823d961b913d0f1149b6
KirutoChan <kkristinaityte@gmail.com>
2017-04-20 19:02:01 +0300

    make some cosmetic changes to 3rd exercise

--- leo3.py
+++ leo3.py
@@ -14,26 +14,25 @@ def prime_numbers(n):
 	return list
 
 def prime(n):
-	product = 1
-	list_prime = prime_numbers(n)
-
 	prime_factors = []
-	for i in list_prime:
+	for i in prime_numbers(n):
 		new = n / i
 		if n % i == 0 and i != 1:
 			prime_factors.append(i)
 			n = new
 	if prime_factors == []:				# checks if the number is already prime itself
 		return [n]
-	if n % 2 != 0:						# because we check only that prime numbers which are under sqrt(n), the last n isn's compared to list_prime (it's larger), so we check if it is prime or not, and if yes, add to prime factors list
+	if n % 2 != 0 and n != 1:			# because we check only that prime numbers which are under sqrt(n), the last n isn's compared to list_prime (it's larger), so we check if it is prime or not, and if yes, add to prime factors list
 		prime_factors.append(int(n))
-	return prime_factors, max(prime_factors)
+	print (prime_factors)
+	
+	return max(prime_factors)
 
 
 print (prime(600851475143)) 
 print (prime(100000000002)) 
 print (prime(1002)) 
-print (prime(6)) 
+print (prime(15)) 
 
 
 
